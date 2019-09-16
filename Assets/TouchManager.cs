ef2de66c1f36e48f20f1c55e71e0a4a70ec794f5
Aron Polner <aron.polner@gmail.com>
2019-09-16 15:25:15 +0200

    multiple linerenderers draw multiple lines - working

--- Assets/TouchManager.cs
+++ Assets/TouchManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARRaycastManager))]
@@ -8,19 +10,23 @@ public class TouchManager : MonoBehaviour
     public GameObject planeToRaycastAgainstPrefab;
     public GameObject objectToRaycastPrefab;
     public Camera arCamera;
+    public GameObject lineRendererPrefab;
 
     private List<ARRaycastHit> hitsAR = new List<ARRaycastHit>();
     private RaycastHit hits;
     private ARRaycastManager arRaycastManager;
     private ARPlaneManager arPlaneManager;
     private bool isPlanePlaced = false;
-    private bool isCubePlaced = false;
-    private GameObject placedObject;
+    private List<GameObject> listOfPlacedObjects;
+    private List<GameObject> listOfLinerenderers;
+    //private LineRenderer lineRenderer;
 
     void Awake()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
         arPlaneManager = GetComponent<ARPlaneManager>();
+        listOfPlacedObjects = new List<GameObject>();
+        listOfLinerenderers = new List<GameObject>();
     }
 
     bool TryGetTouchPosition(out Touch touch)
@@ -60,7 +66,13 @@ public class TouchManager : MonoBehaviour
     {
         int layerMask = 1 << 8;
 
-        if (!isPlanePlaced)
+        // Disable spawning of stuff by pressing a button
+        if (EventSystem.current.IsPointerOverGameObject(0))
+        {
+            return;
+        }
+
+        else if (!isPlanePlaced)
         {
             if (!TryGetTouchPosition(out Touch touch))
                 return;
@@ -71,7 +83,6 @@ public class TouchManager : MonoBehaviour
                 var hitPose = hitsAR[0].pose;
                 Instantiate(planeToRaycastAgainstPrefab, hitPose.position, hitPose.rotation);
                 isPlanePlaced = true;
-                //arPlaneManager.enabled = false;
                 TogglePlaneDetection();
             }
         }
@@ -80,20 +91,57 @@ public class TouchManager : MonoBehaviour
         {
             if(Input.touchCount > 0)
             {
-                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
                 {
                     Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
                     RaycastHit hitInfo;
 
                     if (Physics.Raycast(ray, out hitInfo, layerMask))
                     {
-                        if(hitInfo.collider.tag != "Cube")
-                        {
-                            placedObject = Instantiate(objectToRaycastPrefab, hitInfo.point, Quaternion.identity);
-                        }
+                        var gameObject = Instantiate(objectToRaycastPrefab, hitInfo.point, Quaternion.identity);
+                        listOfPlacedObjects.Add(gameObject);
+                        DrawLinesBetweenObjects();
                     }
                 }
             }
         }
+    }
+
+    private void DrawLinesBetweenObjects()
+    {
+        int lengthOfList = listOfPlacedObjects.Count;
+        if (lengthOfList > 1)
+        {
+            for (int i = 0; i < lengthOfList-1; i++)
+            {
+                try
+                {
+                    var lineRendererGameObject = Instantiate(lineRendererPrefab);
+                    var lineRenderer = lineRendererGameObject.GetComponent<LineRenderer>();
+                    lineRenderer.SetPosition(0, listOfPlacedObjects[i].transform.position);
+                    lineRenderer.SetPosition(1, listOfPlacedObjects[i+1].transform.position);
+                    listOfLinerenderers.Add(lineRendererGameObject);
+                }
+                catch (Exception)
+                {
+                    Debug.LogError("Exceptions baby!");
+                    throw;
+                }
+            }
+        }
+    }
+
+    public void ClearListOfObjects()
+    {
+        // destroy linerenderers
+        for (int i = 0; i < listOfLinerenderers.Count; i++)
+        {
+            Destroy(listOfLinerenderers[i].gameObject);
+        }   listOfLinerenderers.Clear();
+        // destroy the placed objects if any
+        for (int i = 0; i < listOfPlacedObjects.Count; i++)
+        {
+            Destroy(listOfPlacedObjects[i].gameObject);
+        }   listOfPlacedObjects.Clear();
     }
 }
